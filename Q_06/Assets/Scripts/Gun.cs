21fbc7417d1339c97e58da01a9df74eb6da395c1
KimSesame <kimbs082787@gmail.com>
2024-11-19 15:13:44 +0900

    6번 문제 제출

--- Q_06/Assets/Scripts/Gun.cs
+++ Q_06/Assets/Scripts/Gun.cs
@@ -10,9 +10,10 @@ public class Gun : MonoBehaviour
     
     public void Fire(Transform origin)
     {
-        Ray ray = new(origin.position, Vector3.forward);
+        Ray ray = new(origin.position, transform.forward);
         RaycastHit hit;
 
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
         if (Physics.Raycast(ray, out hit, _range, _targetLayer))
         {
             Debug.Log($"{hit.transform.name} Hit!!");
