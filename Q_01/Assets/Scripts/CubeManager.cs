526390577d62c87e16bca8f1ab3fe5cc1037277c
KimSesame <kimbs082787@gmail.com>
2024-11-19 11:29:39 +0900

    1번 문제 제출

--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -11,12 +11,12 @@ public class CubeManager : MonoBehaviour
 
     private void Awake()
     {
-        SetCubePosition(3, 0, 3);
+        CreateCube();
     }
 
     private void Start()
     {
-        CreateCube();
+        SetCubePosition(3, 0, 3);
     }
 
     private void SetCubePosition(float x, float y, float z)
@@ -24,7 +24,7 @@ public class CubeManager : MonoBehaviour
         _cubeSetPoint.x = x;
         _cubeSetPoint.y = y;
         _cubeSetPoint.z = z;
-        _cubeController.SetPosition();
+        _cubeController.SetPosition(_cubeSetPoint);
     }
 
     private void CreateCube()
